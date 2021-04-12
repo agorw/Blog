9ae7bddcf922c496cb62d945fb1599c4caf012c7
agorw <=>
2021-04-13 01:48:28 +0200

    update DBAuth inscription user

--- Core/Auth/DBAuth.php
+++ Core/Auth/DBAuth.php
@@ -14,8 +14,9 @@ class DBAuth
         $this->db = $db;
     }
 
-    public function getUserId(){
-        if($this->logged()){
+    public function getUserId()
+    {
+        if ($this->logged()) {
             return $_SESSION['auth'];
         }
         return false;
@@ -23,17 +24,23 @@ class DBAuth
 
     public function login($username, $password)
     {
-       $user =  $this->db->prepare('SELECT * FROM users WHERE name = ?', [$username], null, true);
-        if($user){
-           if (password_verify($password,$user->password)){
+        $user =  $this->db->prepare('SELECT * FROM users WHERE name = ?', [$username], null, true);
+        if ($user) {
+            if (password_verify($password, $user->password)) {
                 $_SESSION['auth'] = $user->id;
                 return true;
             }
         }
-            return false;
+        return false;
     }
 
-    public function logged(){
+    public function inscription($username, $password)
+    {
+        $this->db->prepare("INSERT INTO `users` (`id`, `name`, `password`) VALUES (NULL, ?, ? )", array($username, password_hash($password, PASSWORD_DEFAULT)));
+    }
+
+    public function logged()
+    {
         return isset($_SESSION['auth']);
     }
 }
